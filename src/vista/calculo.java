05f6889d837f5aec1fc1d41d634f55cfd0ebbd18
elicho68 <elicho68@gmail.com>
2024-08-03 23:31:17 -0600

    primer_commit

---
+++ src/vista/calculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Elicho
+ */
+public class calculo {
+    public static void main(String[] args){
+    
+        System.out.println(18.0%1);
+    
+    }
+    
+}
